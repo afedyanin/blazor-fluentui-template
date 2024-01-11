8deea6aa0318d6c4d14cc943db13733772c90ec5
Anatoly Fedyanin <anfemail@gmail.com>
2024-01-11 14:14:11 +0300

    client fix

--- src/BlazorTemplate.Client/Program.cs
+++ src/BlazorTemplate.Client/Program.cs
@@ -1,7 +1,21 @@
+using BlazorTemplate.Components;
+using BlazorTemplate.Components.Infrastructure;
+using BlazorTemplate.Shared.SampleData;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluentUIComponents();
+builder.Services.AddFluentUIDemoServices();
+
+builder.Services.AddScoped<DataSource>();
 
 await builder.Build().RunAsync();
